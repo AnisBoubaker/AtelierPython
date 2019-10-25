7c860b9e7bfa2779223b3c085ab8e13a7c2ee363
Anis Boubaker <anis@boubaker.ca>
2019-10-25 11:18:26 -0400

    Done: Project

--- __sc/renaud_bray.py
+++ __sc/renaud_bray.py
@@ -6,22 +6,25 @@ import requests
 def scrape(url):
 	contenu = requests.get(url)
 
+	# En cas d'erreur de connexion, on lève une exception ValueError
 	if contenu.status_code != 200:
 		raise ValueError('Accès impossible au site.')
 
 	soup = BeautifulSoup(contenu.content, 'html.parser')
-	print(contenu.content)
-	liste_prix = soup.select("table.pRowCtrl tbody tr td.pmWi div.cont div.divPrice span")
-
+	
+	# Récupération de la liste de tous les prix de la page
+	liste_prix = soup.select("table.pRowCtrl tr td.pmWi div.cont div.divPrice span.lblPrice")
 	prix_reels = []
 	for prix in liste_prix:
 		prix_reels+=[float( prix.string[:-2].replace(",", ".") )]
 
-	liste_titres = soup.select("table.pRowCtrl tbody tr td.pmWi table.prod tbody tr td a")
+	# Ensuite on récupère les titres
+	liste_titres = soup.select("table.pRowCtrl tr td.pmWi table.prod tr td a")
 	titres_reels = []
 	for titre in liste_titres:
 		titres_reels += [titre.string]
 
+	# Finalement, on se crée un beau dictionnaire contenant les titres comme clé et les prix comme valeur
 	ouvrages = zip(titres_reels, prix_reels)
 	return dict(ouvrages)
 
