f5d8ab8aeb42030ae24e0e0fc956b3ec4c898433
Anis Boubaker <anis@boubaker.ca>
2019-10-24 23:10:02 -0400

    WIP: __sc

--- __sc/leslibraires.py
+++ __sc/leslibraires.py
@@ -3,29 +3,33 @@
 from bs4 import BeautifulSoup
 import requests
 
+#url = "https://www.leslibraires.ca/recherche/?s=michel+tremblay&p=1"
 
-url = "https://www.leslibraires.ca/recherche/?s=michel+tremblay&p=1"
-contenu = requests.get(url)
+def scrape(url):
+	contenu = requests.get(url)
 
-if contenu.status_code != 200:
-	exit()
+	if contenu.status_code != 200:
+		raise ValueError('Accès impossible au site.')
 
-soup = BeautifulSoup(contenu.content, 'html.parser')
-liste_prix = soup.select("li.book div.book-desc ul li h4 span.price")
+	soup = BeautifulSoup(contenu.content, 'html.parser')
+	liste_prix = soup.select("li.book div.book-desc ul li h4 span.price")
 
-prix_reels = []
-for prix in liste_prix:
-	prix_reels+=[float( prix.string[:-2].replace(",", ".") )]
+	prix_reels = []
+	for prix in liste_prix:
+		prix_reels+=[float( prix.string[:-2].replace(",", ".") )]
 
-liste_titres = soup.select("li.book div.book-desc h2 a")
-titres_reels = []
-for titre in liste_titres:
-	titres_reels += [titre.string]
+	liste_titres = soup.select("li.book div.book-desc h2 a")
+	titres_reels = []
+	for titre in liste_titres:
+		titres_reels += [titre.string]
 
-print(prix_reels)
-print(titres_reels)
+	ouvrages = zip(titres_reels, prix_reels)
+	return dict(ouvrages)
 
-with open("titres.txt", "w") as fichier:
-	for titre in titres_reels:
-		fichier.write(titre + '\n')
+def __main():
+	livres = scrape("https://www.leslibraires.ca/recherche/?s=michel+tremblay&p=1");
+	print(livres)
 
+
+if __name__ == "__main__":
+	__main()
